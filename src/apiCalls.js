28671c51608e6edef814d19e1a2be7ac1014d75d
David Swatzell <thatsstrikethree@gmail.com>
2024-06-10 11:00:38 -0500

    add a post request function for adding an order

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -6,3 +6,19 @@ export const getOrders = async () => {
   const data = await response.json();
   return data.orders;
 };
+
+export const addOrder = async (order) => {
+  const response = await fetch("http://localhost:3001/api/v1/orders", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(order),
+  });
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  const newOrder = await response.json();
+  return newOrder;
+};
+
