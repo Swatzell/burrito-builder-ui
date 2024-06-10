5800f681de6cf443356331ac9c2a200dced6ae2e
David Swatzell <thatsstrikethree@gmail.com>
2024-06-10 10:50:18 -0500

    add initial apicall for getting orders

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,3 +1,14 @@
-export const getOrders = () => {
-  return fetch("http://localhost:3001/api/v1/orders").then((response) => response.json());
+export const getOrders = async () => {
+  return fetch("http://localhost:3001/api/v1/orders")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error('Failed to get orders');
+    }
+    return response.json();
+  })
+  .then((data) => data.orders)
+  .catch((error) => {
+    console.error("Error getting orders:", error);
+    throw error;
+  });
 };
