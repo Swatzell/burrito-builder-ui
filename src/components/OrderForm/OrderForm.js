37604a5a74d6e8ea222d4617510fb0f08c48b070
David Swatzell <thatsstrikethree@gmail.com>
2024-06-10 10:34:38 -0500

    add setName usestate to the empty onChange

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -52,7 +52,7 @@ function OrderForm(props) {
         placeholder="Name"
         name="name"
         value={name}
-        onChange={(e) => }
+        onChange={(e) => setName(e.target.value)}
       />
 
       {ingredientButtons}
